372f6d4adafcaf09ee86e7416963024a78162d5a
Tim Wood <washwithcare@gmail.com>
2015-10-04 21:14:32 -0500

    Initial Commit

---
+++ ColorSpaces/ColorSpaces.h
@@ -0,0 +1,19 @@
+//
+//  ColorSpaces.h
+//  ColorSpaces
+//
+//  Created by Tim Wood on 10/4/15.
+//  Copyright © 2015 Tim Wood. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ColorSpaces.
+FOUNDATION_EXPORT double ColorSpacesVersionNumber;
+
+//! Project version string for ColorSpaces.
+FOUNDATION_EXPORT const unsigned char ColorSpacesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorSpaces/PublicHeader.h>
+
+
